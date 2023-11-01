dbc9a62fe594372203dfbb74fbb9ba3bb157e7bd
Nitu <manisha9681053451@gmail.com>
2023-11-01 22:45:40 +0530

    reactjs

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import './custom.css'
+import Form from './component/Form/Form';
+
+
+function App() {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
